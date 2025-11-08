dc280caf9c95ad81ee5c5ef2251870ba84c6aa1e
SnipeZilla <cedde@msn.com>
2025-11-08 14:38:22 -0500

    Initial commit

---
+++ Plugins/source/HLstatsZ/HLstatsZ.cs
@@ -0,0 +1,514 @@
+using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
+using Microsoft.Extensions.Logging;
+using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Core.Plugin;
+using CounterStrikeSharp.API.Modules.Commands;
+using CounterStrikeSharp.API.Modules.Utils;
+using CounterStrikeSharp.API.Modules.Menu;
+using CounterStrikeSharp.API.Modules.Entities;
+using CounterStrikeSharp.API.Modules.Events;
+using CounterStrikeSharp.API.Modules.Cvars;
+using CounterStrikeSharp.API.Modules.Timers;
+using GameTimer = CounterStrikeSharp.API.Modules.Timers.Timer; 
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace HLstatsZ;
+
+public class HLstatsZConfig : IBasePluginConfig
+{
+    [JsonPropertyName("Log_Address")] public string Log_Address { get; set; } = "127.0.0.1";
+    [JsonPropertyName("Log_Port")] public int Log_Port { get; set; } = 27500;
+    [JsonPropertyName("BroadcastAll")] public int BroadcastAll { get; set; } = 0;
+    [JsonPropertyName("ServerAddr")] public string ServerAddr { get; set; } = "";
+    public int Version { get; set; } = 1;
+}
+
+public class HLstatsZ : BasePlugin, IPluginConfig<HLstatsZConfig>
+{
+    public static HLstatsZ? Instance;
+    public HLstatsZConfig Config { get; set; } = new();
+
+    private static readonly HttpClient httpClient = new();
+
+    public string Trunc(string s, int max=20)
+        => s.Length > max ? s.Substring(0, Math.Max(0, max - 3)) + "..." : s;
+
+    private string? _lastPsayHash;
+
+    public override string ModuleName => "HLstatsZ";
+    public override string ModuleVersion => "1.5.0";
+    public override string ModuleAuthor => "SnipeZilla";
+
+    public void OnConfigParsed(HLstatsZConfig config)
+    {
+        Config = config;
+        Console.WriteLine($"[HLstatsZ] Config loaded: {Config.Log_Address}:{Config.Log_Port}");
+    }
+
+    private HLZMenuManager _menuManager = null!;
+
+    public override void Load(bool hotReload)
+    {
+        Instance = this;
+
+        RegisterListener<Listeners.OnTick>(OnTick);
+
+        RegisterEventHandler<EventRoundMvp>(OnRoundMvp);
+        RegisterEventHandler<EventBombAbortplant>(OnBombAbortplant);
+        RegisterEventHandler<EventBombDefused>(OnBombDefused);
+        RegisterEventHandler<EventPlayerDisconnect>(OnPlayerDisconnect);
+        RegisterEventHandler<EventPlayerDeath>(OnPlayerDeath);
+
+        AddCommandListener(null, ComamndListenerHandler, HookMode.Pre);
+
+        _menuManager = new HLZMenuManager(this);
+
+        var serverAddr = Config.ServerAddr;
+        if (string.IsNullOrWhiteSpace(serverAddr))
+        {
+            var hostPort = ConVar.Find("hostport")?.GetPrimitiveValue<int>() ?? 27015;
+            var serverIP = GetLocalIPAddress();
+            serverAddr = $"{serverIP}:{hostPort}";
+            Config.ServerAddr=serverAddr;
+        }
+
+    }
+
+    public override void Unload(bool hotReload)
+    {
+        RemoveListener<Listeners.OnTick>(OnTick);
+
+        DeregisterEventHandler<EventRoundMvp>(OnRoundMvp);
+        DeregisterEventHandler<EventBombAbortplant>(OnBombAbortplant);
+        DeregisterEventHandler<EventBombDefused>(OnBombDefused);
+        DeregisterEventHandler<EventPlayerDisconnect>(OnPlayerDisconnect);
+        DeregisterEventHandler<EventPlayerDeath>(OnPlayerDeath);
+
+        RemoveCommandListener(null!, ComamndListenerHandler, HookMode.Pre);
+    }
+
+    // ------------------ Core Logic ------------------
+    public static string GetLocalIPAddress()
+    {
+        using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0);
+        socket.Connect("8.8.8.8", 65530); // Google's DNS
+        var endPoint = socket.LocalEndPoint as IPEndPoint;
+        return endPoint?.Address.ToString() ?? "127.0.0.1";
+    }
+
+    public async Task SendLog(CCSPlayerController player, string message, string verb)
+    {
+        if (!player.IsValid) return;
+        var name    = player.PlayerName;
+        var userid  = player.UserId;
+        var steamid = (uint)(player.SteamID - 76561197960265728);
+        var team    = player.TeamNum switch {2 => "TERRORIST", 3 => "CT", _ => "UNASSIGNED"};
+
+        var serverAddr = Config.ServerAddr;
+
+        var logLine = $"L {DateTime.Now:MM/dd/yyyy - HH:mm:ss}: \"{name}<{userid}><[U:1:{steamid}]><{team}>\" {verb} \"{message}\"";
+
+        try
+        {
+            if (!httpClient.DefaultRequestHeaders.Contains("X-Server-Addr"))
+                httpClient.DefaultRequestHeaders.Add("X-Server-Addr", serverAddr);
+
+            var content = new StringContent(logLine, Encoding.UTF8, "text/plain");
+            var response = await httpClient.PostAsync($"http://{Config.Log_Address}:{Config.Log_Port}/log", content);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                Instance?.Logger.LogInformation($"[HLstatsZ] HTTP log send failed: {response.StatusCode} - {response.ReasonPhrase}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Instance?.Logger.LogInformation($"[HLstatsZ] HTTP log send exception: {ex.Message}");
+        }
+    }
+
+    public static void DispatchHLXEvent(string type, CCSPlayerController? player, string message)
+    {
+        if (Instance == null || player == null) return;
+
+        switch (type)
+        {
+            case "psay":
+                SendPrivateChat(player, message);
+                break;
+            case "csay":
+                Instance.BroadcastCenterMessage(message);
+                break;
+            case "msay":
+                Instance._menuManager.Open(player,message);
+                break;
+            case "say":
+                SendChatToAll(message);
+                break;
+            case "hint":
+                ShowHintMessage(player, message);
+                break;
+            default:
+                player.PrintToChat($"Unknown HLX type: {type}");
+                break;
+        }
+    }
+
+    private static string NormalizeName(string name)
+    {
+        var normalized = name.ToLowerInvariant();
+        normalized = new string(normalized
+            .Where(c => !char.IsControl(c) && c != '\u200B' && c != '\u200C' && c != '\u200D')
+            .ToArray());
+        normalized = new string(normalized.Where(c => c <= 127).ToArray());
+        return normalized.Trim();
+    }
+
+    public static CCSPlayerController? FindTarget(object pl)
+    {
+        string? token = pl as string ?? pl?.ToString();
+        if (string.IsNullOrWhiteSpace(token)) return null;
+
+        // #userid
+        if (token[0] == '#' && int.TryParse(token.AsSpan(1), out var uid))
+            return Utilities.GetPlayers().FirstOrDefault(p => p?.IsValid == true && p.UserId == uid);
+
+        // userid
+        if (int.TryParse(token, out var uid2))
+            return Utilities.GetPlayers().FirstOrDefault(p => p?.IsValid == true && p.UserId == uid2);
+
+        // SteamID64
+        if (ulong.TryParse(token, out var sid64) && token.Length >= 17)
+            return Utilities.GetPlayers().FirstOrDefault(p => p?.IsValid == true && p.SteamID == sid64);
+
+        // Name
+        var name = NormalizeName(token);
+
+        var players = Utilities.GetPlayers().Where(p => p?.IsValid == true).ToList();
+
+        // 1. Exact match
+        var exactMatches = players.Where(p => NormalizeName(p.PlayerName) == name).ToList();
+        if (exactMatches.Count == 1) return exactMatches[0];
+        if (exactMatches.Count > 1) return null;
+
+        // 2. Unique "starts with"
+        var prefixMatches = players.Where(p => NormalizeName(p.PlayerName).StartsWith(name)).ToList();
+        if (prefixMatches.Count == 1) return prefixMatches[0];
+        if (prefixMatches.Count > 1) return null;
+
+        // 3. Unique "contains"
+        var containsMatches = players.Where(p => NormalizeName(p.PlayerName).Contains(name)).ToList();
+        if (containsMatches.Count == 1) return containsMatches[0];
+        if (containsMatches.Count > 1) return null;
+
+        return null;
+    }
+
+    public static void SendPrivateChat(CCSPlayerController player, string message)
+    {
+        player.PrintToChat($"{message}");
+    }
+
+    public static void SendChatToAll(string message)
+    {
+        var players = Utilities.GetPlayers();
+        foreach (var player in players)
+        {
+            if (player?.IsValid == true && player?.IsBot == false)
+            {
+                player.PrintToChat($"{message}");
+            }
+        }
+    }
+
+    public void BroadcastCenterMessage(string message, float durationInSeconds = 5.0f)
+    {
+        string messageHTML = message.Replace("HLstatsZ","<font color='#FFFFFF'>HLstats</font><font color='#FF2A2A'>Z</font>");
+        string messageCHAT = message.Replace("HLstatsZ", "HLstats\x07Z\x01");
+
+        string htmlContent = $"<font color='#FFFFFF'>{messageHTML}</font>";
+
+        var menu = new CenterHtmlMenu(htmlContent, this)
+        {
+            ExitButton = false
+        };
+
+        foreach (var p in Utilities.GetPlayers())
+        {
+            if (p?.IsValid == true && p?.IsBot == false)
+            {
+                if (!_menuManager._activeMenus.ContainsKey(p.SteamID))
+                {
+                    menu.Open(p);
+                } else {
+                    p.PrintToChat($"{messageCHAT}");
+                }
+            }
+        }
+
+        _ = new GameTimer(durationInSeconds, () =>
+        {
+            foreach (var p in Utilities.GetPlayers())
+            {
+                if (p?.IsValid == true && p?.IsBot == false && !_menuManager._activeMenus.ContainsKey(p.SteamID))
+                    MenuManager.CloseActiveMenu(p);
+            }
+        });
+    }
+
+    public static void ShowHintMessage(CCSPlayerController player, string message)
+    {
+        player.PrintToCenter($"{message}");
+    }
+
+    // ------------------ Listener -----------------
+    private HookResult ComamndListenerHandler(CCSPlayerController? player, CommandInfo info)
+    {
+        if (player == null || !player.IsValid)
+            return HookResult.Continue;
+
+        bool MenuIsOpen = _menuManager._activeMenus.ContainsKey(player.SteamID);
+        var command = info.ArgByIndex(0).ToLower();
+
+        if (MenuIsOpen && command == "single_player_pause")
+            Instance!._menuManager.DestroyMenu(player);
+
+        if (!command.StartsWith("say"))
+            return HookResult.Continue;
+
+        var raw = info.ArgCount > 1 ? (info.GetArg(1) ?? string.Empty) : string.Empty;
+        raw = raw.Trim();
+
+        if (raw.Length >= 2 && raw[0] == '"' && raw[^1] == '"')
+            raw = raw.Substring(1, raw.Length - 2);
+
+        bool silenced = raw.StartsWith("/");
+        bool prefixed = raw.StartsWith("!") || silenced;
+        string text = prefixed ? raw.Substring(1) : raw;
+
+        var parts = text.Split(' ', 4, StringSplitOptions.RemoveEmptyEntries);
+        if (parts.Length == 0) return HookResult.Continue;
+
+        var cmd = parts[0].ToLowerInvariant();
+        var args  = parts.Length > 1 ? parts[1].Trim() : "";
+        // ----- HLstatsZ -> Daemon -----
+        if (silenced && parts.Length == 1)
+        {
+            switch (cmd)
+            {
+                case "rank":
+                case "next":
+                case "top10":
+                case "top20":
+                case "top30":
+                case "session":
+                    _ = SendLog(player, cmd, "say");
+                return HookResult.Handled;
+                default:
+                break;
+            }
+        }
+
+        // ---- Handle Public Command ----
+        if ((cmd == "menu" || cmd == "hlx_menu") && parts.Length == 1)
+        {
+            if (MenuIsOpen)
+                Instance!._menuManager.DestroyMenu(player);
+
+            var builder = new HLZMenuBuilder("Main Menu");
+
+            builder.Add("Rank", p => _ = SendLog(p, "rank", "say"));
+            builder.Add("Next Rank", p => _ = SendLog(p, "next", "say"));
+            builder.Add("TOP 10", p => _ = SendLog(p, "top10", "say"));
+
+            builder.Open(player, Instance!._menuManager);
+
+            return HookResult.Handled;
+        }
+
+        return HookResult.Continue;
+    }
+
+    // --------------------- Console ---------------------
+    [ConsoleCommand("hlx_sm_psay")]
+    public void OnHlxSmPsayCommand(CCSPlayerController? _, CommandInfo command)
+    {
+        if (command.ArgCount < 2) return; // hlx_sm_psay "1" 1 "message"
+
+        var arg = command.ArgByIndex(1);
+        var message = command.ArgByIndex(command.ArgCount - 1);
+
+        string[] privateOnlyPatterns =
+        {
+            "kills to get regular points",
+            "You have been banned"
+        };
+        bool isPrivateOnly = privateOnlyPatterns.Any(p => message?.IndexOf(p, StringComparison.OrdinalIgnoreCase) >= 0);
+        // Broadcast to all
+        if (Config.BroadcastAll == 1 && !isPrivateOnly)
+        {
+            var hash = $"ALL:{message}";
+            if (_lastPsayHash == hash) return;
+
+            _lastPsayHash = hash;
+            DispatchHLXEvent("say", null, message);
+            return;
+        }
+
+        // users?
+        var userIds = new List<int>();
+        foreach (var idStr in arg.Split(','))
+        {
+            if (int.TryParse(idStr, out var id)) userIds.Add(id);
+        }
+
+        // Broadcast to user
+        foreach (var userid in userIds)
+        {
+            var target = FindTarget(userid);
+            if (target == null || !target.IsValid) continue;
+
+            var hash = $"{userid}:{message}";
+            if (_lastPsayHash == hash) continue;
+
+            _lastPsayHash = hash;
+            DispatchHLXEvent("psay", target, message);
+        }
+    }
+
+    [ConsoleCommand("hlx_sm_csay")]
+    public void OnHlxSmCsayCommand(CCSPlayerController? _, CommandInfo command)
+    {
+        var message = command.ArgByIndex(1);
+        DispatchHLXEvent("csay", null, message);
+    }
+
+    [ConsoleCommand("hlx_sm_hint")]
+    public void OnHlxSmHintCommand(CCSPlayerController? _, CommandInfo command)
+    {
+        if (!int.TryParse(command.ArgByIndex(1), out var userid)) return;
+
+        var message = command.ArgByIndex(command.ArgCount - 1);
+        var target  = FindTarget(userid);
+        if (target == null || !target.IsValid) return;
+        DispatchHLXEvent("hint", target, message);
+    }
+
+    [ConsoleCommand("hlx_sm_msay")]
+    public void OnHlxSmMsayCommand(CCSPlayerController? _, CommandInfo command)
+    {
+        if (!int.TryParse(command.ArgByIndex(2), out var userid)) return;
+
+        var message = command.ArgByIndex(command.ArgCount - 1);
+        var target  = FindTarget(userid);
+        if (target == null || !target.IsValid) return;
+        DispatchHLXEvent("msay", target, message);
+    }
+
+    // --------------------- Menu ---------------------
+    private const int PollInterval = 6; // 4~80ms 6~120ms
+    private int _tickCounter = 0;
+
+    private void OnTick()
+   {
+        if (_menuManager._activeMenus.Count == 0) return;
+        if (++_tickCounter % PollInterval != 0) return;
+        _tickCounter=0;
+
+        foreach (var kvp in _menuManager._activeMenus.ToList())
+        {
+            var steamId = kvp.Key;
+            var (player, menu) = kvp.Value;
+
+            if (player == null || !player.IsValid)
+            {
+                _menuManager.DestroyMenu(player!);
+                continue;
+            }
+            var current = player.Buttons;
+            var last = _menuManager._lastButtons.TryGetValue(steamId, out var prev) ? prev : PlayerButtons.Cancel;
+
+            if (current.HasFlag(PlayerButtons.Forward) && !last.HasFlag(PlayerButtons.Forward))
+                _menuManager.HandleWasdPress(player, "W");
+
+            if (current.HasFlag(PlayerButtons.Back) && !last.HasFlag(PlayerButtons.Back))
+                _menuManager.HandleWasdPress(player, "S");
+
+            if (current.HasFlag(PlayerButtons.Moveleft) && !last.HasFlag(PlayerButtons.Moveleft))
+                _menuManager.HandleWasdPress(player, "A");
+
+            if (current.HasFlag(PlayerButtons.Moveright) && !last.HasFlag(PlayerButtons.Moveright))
+                _menuManager.HandleWasdPress(player, "D");
+
+            if (current.HasFlag(PlayerButtons.Use) && !last.HasFlag(PlayerButtons.Use))
+                _menuManager.HandleWasdPress(player, "E");
+
+            _menuManager._lastButtons[steamId] = current;
+
+        }
+    }
+
+    // ------------------ Event Handler ------------------
+    public HookResult OnRoundMvp(EventRoundMvp @event, GameEventInfo info)
+    {
+        var reason = @event.Reason;
+        var player = @event.Userid;
+        if (player == null || !player.IsValid) return HookResult.Continue;
+
+        string reasonText = reason switch
+        {
+            1  => "with most eliminations",
+            2  => "with bomb planted",
+            3  => "with bomb defused",
+            4  => "with hostage rescued",
+            11 => "with HE grenade",
+            14 => "with a clutch defuse",
+            15 => "with most kills",
+            _  => $"with best overall"
+        };
+
+        _ = SendLog(player, $"round_mvp {reasonText}", "triggered");
+        return HookResult.Continue;
+    }
+
+    public HookResult OnBombAbortplant(EventBombAbortplant @event, GameEventInfo info)
+    {
+        var player = @event.Userid;
+        if (player == null || !player.IsValid) return HookResult.Continue;
+
+        _ = SendLog(player, "Aborted_Bomb", "triggered");
+        return HookResult.Continue;
+    }
+
+    public HookResult OnBombDefused(EventBombDefused @event, GameEventInfo info)
+    {
+        var player = @event.Userid;
+        if (player == null || !player.IsValid) return HookResult.Continue;
+
+        _ = SendLog(player, "Defused_The_Bomb", "triggered");
+        return HookResult.Continue;
+    }
+
+    public HookResult OnPlayerDisconnect(EventPlayerDisconnect @event, GameEventInfo info)
+    {
+        var player = @event.Userid;
+        if (player != null && player.IsValid)
+            _menuManager.DestroyMenu(player);
+        return HookResult.Continue;
+    }
+
+    public HookResult OnPlayerDeath(EventPlayerDeath @event, GameEventInfo info)
+    {
+        var player = @event.Userid;
+        if (player != null && player.IsValid)
+            _menuManager.DestroyMenu(player);
+        return HookResult.Continue;
+    }
+
+}
+
---
+++ Readme.md
@@ -0,0 +1,45 @@
+## HLstatsZ Plugin for CounterStrikeSharp (CS2) 🎯
+This plugin extends CounterStrikeSharp to enable full(mostly) HLstatsX:CE support on CS2 servers.
+
+## ✨ Overview
+* This plugin emulates the classic `hlstats.smx` behavior, allowing CounterStrikeSharp CS2 servers to communicate seamlessly with HLstatsX backends.
+* It receives RCON commands from your daemon (`HLX_SM_XX`) and also dispatches enriched event logs via HTTP
+
+## 🔧 Key Features
+- ☝ Spoofs SOURCEMOD's hlstats.smx; Server Mod set as 'SOURCEMOD', Events unchanged hlx_sm_xx 
+- ✅ Sends additional HTTP game event which are not logged by cs2
+- 🚀 Optimized for modern CS2 servers and the revamped HLstatsX ecosystem
+- 🧩️ Minimal performance and Fully configurable
+- 😻 WASDE menu
+<img width="254" height="150" alt="image" src="https://github.com/user-attachments/assets/2a2de88a-bef5-471b-9b3e-59f824e911b0" />
+<img width="255" height="154" alt="image" src="https://github.com/user-attachments/assets/d1b60635-650d-4334-a0ca-2149589f1bef" />
+<img width="253" height="152" alt="image" src="https://github.com/user-attachments/assets/3f469803-93da-484b-87f1-08316cee8a76" />
+
+## ⚙️ Config
+# Easy server setup—just two config changes
+- CS2 x-server.cfg:
+```
+log on
+logaddress_add_http "http://127.0.0.1:27500"
+sv_visiblemaxplayers 32 // RCON status does not report max players
+```
+-  CounterStrikeSharp\Configs\Plugins\HLstatsZ\HLstatsZ.json:
+  
+```
+  "Log_Address": "127.0.0.1",
+  "Log_Port": 27500,
+  "BroadcastAll": 0,
+  "ServerAddr":"64.74.97.164:27015"
+```
+✅ Works with the updated HLxce daemon version >= 2.3.4, which supports both async UDP and HTTP log ingestion on the same port.
+
+* Log_Port: The UDP/HTTP port your daemon is listening on (port needs to be open for both UDP and TCP)
+
+* BroadcastAll: 1 to Emulate old hlstatsx.smx (tf2,css...), 0 for sourcemod csgo/cs2
+
+* ServerAddr: force ip:port if you see not authorized in the log. still highly recommended to force log on known ip:port
+
+
+
+
+
